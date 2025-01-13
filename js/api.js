da8195c4c37deb6498ccf6cd5c6f023826176a02
yongjun ko <mbdyjk@gmail.com>
2025-01-13 15:48:10 +0900

    feat: 인기영화 목록 가져오는 함수 추가, fetch에 필요한 options 값 얻는 함수 분리

--- js/api.js
+++ js/api.js
@@ -10,16 +10,38 @@ const BASE_URL = 'https://api.themoviedb.org/3';
  * Bearer 토큰 방식
  */
 export async function fetchMoviesExample() {
-    const options = {
-        method: 'GET',
-        headers: {
-            accept: 'application/json',
-            Authorization: `Bearer ${TMDB_API_KEY}`
-        }
-    };
+    const options = getTMDBOptions('GET');
 
     fetch(`${BASE_URL}/authentication`, options)
         .then(res => res.json())
         .then(res => log('Success', 'fetchMoviesExample', 'fetch 완료', res))
         .catch(err => log('Error', 'fetchMoviesExample', 'fetch 실패', err));
+}
+
+/**
+ * 인기 영화 목록을 가져오는 함수
+ * @returns {Promise<Array>} - 인기 영화 목록 배열
+ */
+export async function fetchPopularMovies() {
+    const options = getTMDBOptions('GET');
+
+    fetch('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1', options)
+        .then(res => res.json())
+        .then(res => log('Success', 'fetchPopularMovies', 'fetch 완료', res))
+        .catch(err => log('Error', 'fetchPopularMovies', 'fetch 실패', err));
+}
+
+/**
+ * TMDB API를 호출하기 위한 기본 옵션을 반환하는 함수
+ * @param {string} method - HTTP 메서드 (예: 'GET', 'POST', 'PUT' 등)
+ * @returns {Object} - fetch에 필요한 옵션 객체
+ */
+function getTMDBOptions(method) {
+    return {
+        method: method,
+        headers: {
+            accept: 'application/json',
+            Authorization: `Bearer ${TMDB_API_KEY}`  // Bearer 토큰 방식
+        }
+    };
 }
--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
 // 메인 로직 처리
 import { log } from './utils/logger.js';
-import { fetchMoviesExample } from './api.js';
+import { fetchMoviesExample, fetchPopularMovies } from './api.js';
 
 // 테스트
 document.addEventListener('DOMContentLoaded', () => {
-    fetchMoviesExample();
+    //fetchMoviesExample();
+    fetchPopularMovies();
 });
